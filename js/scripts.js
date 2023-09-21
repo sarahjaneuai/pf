61f58e8a509fed8a8194ca2ac48aeaa82f564009
sarahjaneuai <103144897+sarahjaneuai@users.noreply.github.com>
2023-09-21 14:03:15 -0600

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,18 @@ window.addEventListener('DOMContentLoaded', event => {
     const listHoursArray = document.body.querySelectorAll('.list-hours li');
     listHoursArray[new Date().getDay()].classList.add(('today'));
 })
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
